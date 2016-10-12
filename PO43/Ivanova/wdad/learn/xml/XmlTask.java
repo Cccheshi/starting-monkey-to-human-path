d8f48a443c36b57f7edba00f13cbc1a6a80b920f
cyvorov <stefanov-m@yandex.ru>
2016-10-12 09:09:30 +0400

    aded TODO

--- PO43/Ivanova/wdad/learn/xml/XmlTask.java
+++ PO43/Ivanova/wdad/learn/xml/XmlTask.java
@@ -17,6 +17,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XmlTask {
+    //TODO move to constructor with exception's handling
     File file = new File("C:/Users/User/IdeaProjects/starting-monkey-to-human-path/src/PO43/Ivanova/wdad/learn/xml/Appropriate.xml");
     /* DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
      DocumentBuilder db=dbf.newDocumentBuilder();
@@ -31,6 +32,7 @@ public class XmlTask {
         int salarySum = 0;
         int count = 0;
         for (int j = 0; j < nodeList.getLength(); j++) {
+            //TODO catch NumberFormatException
             salary = Integer.valueOf(nodeList.item(j).getTextContent());
             salarySum += salary;
             count++;
@@ -40,8 +42,15 @@ public class XmlTask {
 
     //возвращает среднюю заработную плату сотрудников организации.
     public int salaryAverage() {
-        NodeList salaries = document.getElementsByTagName("salary");
-        return countSalaryAverage(salaries);
+        return average (document, "salary");
+    }
+
+    private int average(Node node, String param) {
+        if (node.getNodeType()==Node.ELEMENT_NODE) {
+            NodeList salaries = ((Element) node).getElementsByTagName(param);
+            return countSalaryAverage(salaries);
+        } else
+            return 0;
     }
 
     //возвращает среднюю заработную плату сотрудников заданного департамента.
@@ -52,10 +61,10 @@ public class XmlTask {
         for (int i = 0; i < departments.getLength(); i++) {
             department = departments.item(i);
             if (department.getAttributes().item(0).getNodeValue().equals(departmentName)) {
-                departmentSalaries = ((Element) department).getElementsByTagName("salary");
+                return average(department,"salary");
             }
         }
-        return countSalaryAverage(departmentSalaries);
+        return 0;
     }
 
     private Node findEmployee(String firstName, String secondName) {
@@ -65,13 +74,14 @@ public class XmlTask {
         NodeList employee = document.getElementsByTagName(tegEmployee);
         Node node = null;
         for (int i = 0; i < employee.getLength(); i++) {
+            //TODO не делай более 3-х последовательных вызовов
             if (employee.item(i).getAttributes().getNamedItem(attrFirstName).getNodeValue().equals(firstName) && employee.item(i).getAttributes().getNamedItem(attrSecondName).getNodeValue().equals(secondName)) {
                 node = employee.item(i);
                 return node;
             }
 
         }
-        return node;
+        return null;
     }
     private void writeDoc() throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -96,7 +106,7 @@ public class XmlTask {
     //изменяет размер зароботной платы сотрудника.
     public void setSalary(String firstName, String secondName, int newSalary) throws TransformerException {
         String tegSalary = "salary";
-        Node employee=findEmployee(firstName,secondName);
+        Node employee=findEmployee(firstName,secondName); //TODO NullPointerException
         NodeList employeeChild = employee.getChildNodes();
         for (int i = 0; i < employeeChild.getLength(); i++) {
             if (employeeChild.item(i).getNodeName().equals(tegSalary)) {
@@ -108,7 +118,7 @@ public class XmlTask {
 
     // удаляющий информацию о сотруднике.
     public void fireEmployee(String firstName, String secondName) throws TransformerException {
-        Node employee = findEmployee(firstName, secondName);
+        Node employee = findEmployee(firstName, secondName); //TODO NullPointerException
         Node parent=employee.getParentNode();
         parent.removeChild(employee);
         writeDoc();
